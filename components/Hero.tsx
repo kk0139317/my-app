f701ac6f70c928573ca01ebaf8a5ed1f736768bb
kk0139317 <kk4881757@gmail.com>
2024-05-22 15:47:39 +0530

    Changes to be committed: 	new file:   components/Hero.tsx 	new file:   components/Nav.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    // <section>
+        <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800">
+	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+			{/* <img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" /> */}
+            <Image src={''} height={500} width={500} alt='' >
+
+            </Image>
+		</div>
+		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
+				<span className="text-default-400 dark:text-default-600">senectus</span>
+			</h1>
+			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
+				<br  className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
+			</p>
+			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-default-400 dark:bg-default-600 text-gray-900 dark:text-gray-50">Suspendisse</a>
+				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded border-gray-100 dark:border-gray-800">Malesuada</a>
+			</div>
+		</div>
+	</div>
+</section>
+    // </section>
+  )
+}
+
+export default Hero
