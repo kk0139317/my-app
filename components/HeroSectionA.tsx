baae10067f4ef4408ad369b348654d9268ddb2a7
kk0139317 <kk4881757@gmail.com>
2024-05-23 15:11:59 +0530

    renamed:    components/Hero.tsx -> components/HeroSectionA.tsx

--- components/HeroSectionA.tsx
+++ components/HeroSectionA.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800">
 	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
 		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-			{/* <img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" /> */}
+			<img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
             <Image src={''} height={500} width={500} alt='' >
 
             </Image>
